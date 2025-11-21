946d082ead177f9cfab139d55a407e0d267d50e1
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-11-21 12:48:02 +0200

    init main and server.js

---
+++ src/server.js
@@ -0,0 +1,15 @@
+/* import path from 'node:path';
+import fs from 'node:fs';
+
+console.log('File path:', path.join(process.cwd(), 'test.txt'));
+console.log('Parse:', path.parse(process.cwd()));
+
+const mes = 'Hello world';
+
+console.log(mes);
+
+console.log(
+  'Read file:',
+  fs.readFileSync(path.join(process.cwd(), 'test.txt')),
+);
+ */
