059b332efc8563c2107ae3195f775ffbbe3e57bf
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-11-21 13:20:32 +0200

    predeploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,35 @@
-import 'dotenv/config';
-
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+//Time logging:
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
@@ -12,6 +38,27 @@ app.get('/notes', (req, res) => {
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+//Error path fot testing:
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
 });
 
 app.listen(PORT, () => {
