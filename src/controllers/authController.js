ffab386c5938e0e4ca3e8bc210f771a08e1511d3
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-12-10 09:51:45 +0200

    add to auth contoleer hashing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 
@@ -7,6 +8,11 @@ export const registerUser = async (req, res, next) => {
   const existingUser = await User.findOne({ email });
   if (existingUser) return next(createHttpError(400, 'Email in use'));
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ email, password: hashedPassword });
+
   //TODO Adding
-  res.status(201).json({});
+
+  res.status(201).json({ newUser });
 };
