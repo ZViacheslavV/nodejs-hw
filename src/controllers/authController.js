cf373ff7214d91b5c595d5d2bab17d342e01aa1a
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-12-10 09:36:55 +0200

    fix extensions

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,12 @@
 import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
-  if (existingUser) return nexy(createHttpError(400, 'Email in use'));
+  if (existingUser) return next(createHttpError(400, 'Email in use'));
 
+  //TODO Adding
   res.status(201).json({});
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,39 +1,39 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from 'mongoose';
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            trim:true
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
+  {
+    username: {
+      type: String,
+      trim: true,
     },
-    {
-        timestamps: true,
-        versionKey: false
-    }
-)
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 userSchema.pre('save', function (next) {
-    if (!this.username) {
-        this.username = this.email
-    }
-    next();
-})
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
 
 userSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
-}
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
 
 export const User = model('user', userSchema);
