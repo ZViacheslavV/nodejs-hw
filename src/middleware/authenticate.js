217086dea994d34ca5a4e38af5b9c8ebdcc75970
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-12-11 17:26:57 +0200

    auth middle start 2

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,6 +1,11 @@
 import createHttpError from 'http-errors';
+import { Session } from '../models/session.js';
 
 export const authenticate = async (req, res, next) => {
   if (!req.cookies.accessToken)
     throw createHttpError(401, 'Missing access token');
+
+  const session = Session.findOne({
+    accessToken: req.cookies.accessToken,
+  });
 };
