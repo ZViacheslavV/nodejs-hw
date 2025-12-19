e6a81f7dfb6cc4cbc413b12aa13c162eeaab5bbf
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-12-19 23:40:55 +0200

    add user avatar first iteration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,7 @@
+import createHttpError from 'http-errors';
+
 export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) throw createHttpError(400, 'No file');
+
   res.status(200).json({ url: '' });
 }; //TODO
